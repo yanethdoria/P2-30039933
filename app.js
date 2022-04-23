2d59ced46a42e6b1917ed6b6d9098e4f3d7220e5
Yaneth <yanethb.2210@gmail.com>
2022-04-23 10:33:42 -0400

    task-0

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 // Imports
 
 const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-app.listen(PORT, function() {
-	console.log('Escuchando en el puerto: ', PORT)}
+const app = express();
+const server = app.listen(process.env.PORT || 3000);
+const portNumber = server.address().port;
 
 // Static Files
 
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
 
 // Set Views
 
-app.set('views', './views')
-app.set('view engine', 'ejs')
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.get('', (req, res) => {
 	res.render('index')
 })
+
 
